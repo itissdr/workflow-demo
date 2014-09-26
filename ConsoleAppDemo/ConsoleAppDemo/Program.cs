59a9034c160e87c3853259c5b4895bb91a25861c
sdr <itissdr@live.com>
2014-09-26 12:18:40 -0400

    working through a demo workflow

---
+++ ConsoleAppDemo/ConsoleAppDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
